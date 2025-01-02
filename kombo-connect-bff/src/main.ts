3f59a62c44b6cc00b0063dff35bbdcad06127269
camillefauchier <camillef@sipios.com>
2025-01-02 09:52:08 +0100

    fix deployment issue bis

--- kombo-connect-bff/src/main.ts
+++ kombo-connect-bff/src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Enable CORS for frontend communication
-  await app.listen(await app.listen(process.env.PORT || 3001));
+  app.enableCors();
+  const port = process.env.PORT || 3000; // Use// Enable CORS for frontend communication
+  await app.listen(port);
 }
 bootstrap();
