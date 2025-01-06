44a1750efa55f8a759cbe9f844774264a93ed202
camillefauchier <camillef@sipios.com>
2025-01-06 10:57:33 +0100

    api: enable cors

--- kombo-connect-bff/src/main.ts
+++ kombo-connect-bff/src/main.ts
@@ -3,8 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  const port = process.env.PORT || 3001; // Use// Enable CORS for frontend communication
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+  const port = process.env.PORT || 3001;
   await app.listen(port);
 }
 bootstrap();
